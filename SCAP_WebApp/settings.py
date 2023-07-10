fffbe69ca8831ac98aa78b328675037678a7df82
gtondapu <githika.tondapu@nasa.gov>
2023-07-10 15:50:18 -0400

    settings update

--- SCAP_WebApp/settings.py
+++ SCAP_WebApp/settings.py
@@ -11,21 +11,23 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
-
+import json
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+f = open(str(BASE_DIR) + '/data.json', )
+data = json.load(f)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4r1lnv7v#@j@z*-zgpq&p+d())xwmkq3tzs5@%%s25cvn*j592'
+SECRET_KEY = data["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = data["ALLOWED_HOSTS"]
 
+CSRF_TRUSTED_ORIGINS = data["CSRF_TRUSTED_ORIGINS"]
 # Application definition
 
 INSTALLED_APPS = [
