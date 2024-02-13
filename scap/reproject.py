25a68fe1729b8cd93aa26481ea0f6f48e8b8cc14
alex <alex@ubuntu.dev.myguest.virtualbox.org>
2024-02-13 15:48:57 -0600

    Generate Emissions

--- scap/reproject.py
+++ scap/reproject.py
@@ -1,10 +1,17 @@
 import numpy as np
 from osgeo import gdal, osr, gdalconst
+from pathlib import Path
+from rasterio.windows import Window
 import rasterio as rio
 import os
+import json
+import re
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+config = json.load(open(str(BASE_DIR) + '/data.json', ))
 
 def invert_gtiff(source_path):
-    dtype = gdal.GDT_UInt16
+    dtype = gdal.GDT_Byte
 
     filename = source_path.split('/')[-1]
     folder = '/'.join(source_path.split('/')[:-1]) + '/'
@@ -20,7 +27,7 @@ def invert_gtiff(source_path):
     # Prepare destination file
     driver = gdal.GetDriverByName("GTiff")
 
-    options = ["TILED=YES", "BLOCKXSIZE=256", "BLOCKYSIZE=256"]
+    options = ["TILED=YES", "BLOCKXSIZE=256", "BLOCKYSIZE=256", "COMPRESS=LZW"]
 
     if options != 0:
         dest = driver.Create(output_path, width, height, 1, dtype, options)
@@ -42,7 +49,7 @@ def invert_gtiff(source_path):
 
     return output_path
 
-def write_gtiff(source):
+def reproject_gtiff(source):
     """
     Writes a geotiff.
 
@@ -52,8 +59,8 @@ def write_gtiff(source):
     dtype (OPTIONAL): datatype to save as
     nodata (default: FALSE): set to any value you want to use for nodata; if FALSE, nodata is not set
     """
-    match = r"/home/alex/shared/SCAP/agb/Resampled_1ha/global_agb_2000_liu_.tif"
-    dtype = gdal.GDT_UInt16
+    match = r"/home/alex/shared/SCAP/agb/global_agb_2000_liu_.tif"
+    dtype = gdal.GDT_Byte
     nbands = 1
     nodata = -9999
 
@@ -69,14 +76,24 @@ def write_gtiff(source):
     match_proj = match_obj.GetProjection()
 
     gt = match_obj.GetGeoTransform()
+    
+    # CLIP TO PERU FOR DEMO
+    gt = list(gt)
+    decimal_x = float('0.' + str(gt[0]).split('.')[1])
+    decimal_y = 1 - float('0.' + str(gt[3]).split('.')[1])
+    gt[0] = -8150994 - decimal_x
+    gt[3] = -4711 - decimal_y
+    gt = tuple(gt)
 
-    width = match_obj.RasterXSize
-    height = match_obj.RasterYSize
+    # CUT EXTENT TO PERU FOR DEMO
+    width = 14889
+    height = 22496
+
 
     # Prepare destination file
     driver = gdal.GetDriverByName("GTiff")
 
-    options = ["TILED=YES", "BLOCKXSIZE=256", "BLOCKYSIZE=256"]
+    options = ["TILED=YES", "BLOCKXSIZE=256", "BLOCKYSIZE=256", "COMPRESS=LZW"]
 
     if options != 0:
         dest = driver.Create(outputpath, width, height, nbands, dtype, options)
@@ -99,30 +116,48 @@ def write_gtiff(source):
     dest = None
     return outputpath
 
-def calc_emissions(source):
-    # Replace with your actual file paths and desired output name
-    forest_change_path = "/home/alex/shared/SCAP/fcc_working/fcc/esri/INVERTED_REPROJECTED_fcc_esri_peru_2018_1ha.tif"
-    AGB_path = "/home/alex/shared/SCAP/agb/Resampled_1ha/global_agb_2000_liu_.tif"
-    output_path = "/home/alex/shared/SCAP/emissions_test.tif"
 
-    with rio.open(forest_change_path) as src_change, rio.open(AGB_path) as src_AGB:
+def generate_emissions_gtiff(source, agb):
+    # Replace with your actual file paths and desired output name
+    fcc_filename = '_'.join(source.split('/')[-1].split('_')[2:])
+    agb_filename, _ = os.path.splitext(agb.split('/')[-1])
+    agb_year = re.search('\\d{4}', agb_filename)
+    fcc_year = re.search('\\d{4}', fcc_filename)
+    if 'tropics' in agb_filename:
+        print('This AGB does not have proper transform')
+        return
+    if (not (agb_year and fcc_year)) or (agb_year.group(0) > fcc_year.group(0)):
+        print('Not generating emissions; AGB was created after the current year '
+              '{}, {}'.format(agb_filename, fcc_filename))
+        return
+
+    output_path = os.path.join(config['DATA_DIR'], 'emissions', agb_filename, fcc_filename.replace('fcc', 'emissions'))
+
+    with rio.open(source) as src_change, rio.open(agb) as src_AGB:
         # Check if resolutions match
-        if src_change.transform != src_AGB.transform:
-            raise ValueError("Rasters must have the same resolution and transform!")
+        # Resolutions/transforms won't match bc they dont cover same extent
+        #if src_change.transform != src_AGB.transform:
+        #    raise ValueError("Rasters must have the same resolution and transform!")
+
 
         # Create output TIF with matching profile
-        profile = src_AGB.profile.copy()
-        profile.update({'count': 1})  # Update band count to 1 for masked output
+        profile = src_change.profile.copy()
+        profile.update({'count': 1, 'dtype': rio.uint16})  # Update band count to 1 for masked output
         with rio.open(output_path, 'w', **profile) as dst:
 
             for (_, window) in src_change.block_windows(1):
                 # Read blocks of data
                 forest_change_block = src_change.read(1, window=window)
-                AGB_block = src_AGB.read(1, window=window)
+
+                sr = window.row_off + 85323
+                sc = window.col_off + 98901
+                corresponding_window = Window.from_slices((sr, sr+window.height), (sc, sc+window.width))
+                print(corresponding_window)
+
+                AGB_block = src_AGB.read(1, window=corresponding_window)
 
                 # Create mask for the block
-                forest_loss_mask = forest_change_block == 1
-                print(forest_loss_mask.shape)
+                forest_loss_mask = (forest_change_block == 1) * 48
 
                 # Apply mask to AGB block
                 AGB_masked_block = np.multiply(AGB_block, forest_loss_mask)
@@ -131,22 +166,38 @@ def calc_emissions(source):
                 dst.write(AGB_masked_block, window=window, indexes=1)
 
     print(f"Masked AGB values written to {output_path} in blocks")
+    
+def map_to_agbs(source):
+    all_agbs = os.listdir(os.path.join(config['DATA_DIR'], 'agb'))
+    for agb in all_agbs:
+        print('Generating emissions for AGB {} and FCC {}'.format(agb, source))
+        generate_emissions_gtiff(source, os.path.join(config['DATA_DIR'], 'agb', agb))
 
-def reproject_from_root(base_dir):
+def process_directory(base_dir):
     subdirs = os.listdir(base_dir)
     for dir in subdirs:
         if dir != 'cci':
             dir_path = os.path.join(base_dir, dir)
             files = os.listdir(dir_path)
             for file in files:
-                source = os.path.join(dir_path, file)
+                try:
+                    if (not 'tif' in file) and ('xml' not in file):
+                        print('Skipping file {}'.format(file))
+                        continue
+                    source = os.path.join(dir_path, file)
+                    print('Processing file {}'.format(file))
 
-                print("Inverting {}".format(source))
-                source = invert_gtiff(source)
+                    print("Inverting {}".format(source))
+                    source = invert_gtiff(source)
 
-                print("Reprojecting {}".format(source))
-                source = write_gtiff(source)
+                    print("Reprojecting {}".format(source))
+                    source = reproject_gtiff(source)
+
+                    map_to_agbs(source)
+                except:
+                    print('ERROR processing file {}'.format(file))
+                    continue
         print("Done with {}".format(dir))
         print()
 
-reproject_from_root('/home/alex/shared/SCAP/fcc_working/fcc/')
+process_directory(os.path.join(config['DATA_DIR'], 'fcc'))
