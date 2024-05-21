c8fdddde9d346615b73658821e58e632cf561a57
agoberna <alexgoberna@gmail.com>
2024-05-21 13:38:55 -0500

    Delete static directory

--- static/js/datatables.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $('#myTable').DataTable();
-    $('#tifffTable').DataTable();
-});
-$("#myTable").addClass("table table-striped table-sm small display");
-$("#tifffTable").addClass("table table-striped table-sm small display");
