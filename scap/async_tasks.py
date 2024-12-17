e2d0ecb8a6f5c9790c8c363712e2e682ad1a86bc
gtondapu <githika.tondapu@nasa.gov>
2024-12-17 11:43:55 -0500

    boundary file validation bug fix

--- scap/async_tasks.py
+++ scap/async_tasks.py
@@ -76,7 +76,7 @@ def validate_uploaded_dataset(self, dataset_id, dataset_type,coll_name,username,
         boundary_file_exists=False
         if boundary_file is not None:
             boundary_file_exists=True
-            if not validate_file(bytes(boundary_file.file.read()),'fc_boundary'):
+            if validate_file(boundary_file.file,'fc_boundary'):
                 boundary_file_valid = True
 
 
@@ -89,19 +89,42 @@ def validate_uploaded_dataset(self, dataset_id, dataset_type,coll_name,username,
                 result = False
                 break
         if result:
-            existing_coll.approval_status = 'Submitted'
-            existing_coll.processing_status = 'Not Processed'
-            existing_coll.save()
-            if boundary_file_exists and boundary_file_valid:
-                message = (
-                    'Your data along with boundary file passed the initial validation. Submitted for admin review. Please wait for an email that will let you know '
-                    'if the dataset is approved/denied.')
+            # existing_coll.approval_status = 'Submitted'
+            # existing_coll.processing_status = 'Not Processed'
+            # existing_coll.save()
+            if boundary_file_exists:
+                if boundary_file_valid:
+                    message = (
+                        'Your data along with boundary file passed the initial validation. Submitted for admin review. Please wait for an email that will let you know '
+                        'if the dataset is approved/denied.')
+                    send_mail('[S-CAP] - Message about your collection: ' + coll_name, message,
+                              config['EMAIL_HOST_USER'],
+                              [email])
+                    # send notification to scap_sysadmins
+                    approver_group = Group.objects.get(name='scap_sysadmins')
+                    approvers = approver_group.user_set.all()
+                    approvers_emails = [approver.email for approver in approvers if approver.email]
+                    url = 'https://' + config['ALLOWED_HOSTS'][1] + '/validation/fc/'
+                    admin_message = 'Hi,\nUser data passed the initial validation. Please review the collection and take appropriate action. Follow this link in order to review ' + url
+                    send_mail('[S-CAP] - Message about user collection: ' + coll_name, admin_message,
+                              config['EMAIL_HOST_USER'],
+                              approvers_emails)
+                    existing_coll.approval_status = 'Submitted'
+                    existing_coll.processing_status = 'Not Processed'
+                    existing_coll.save()
+                else:
+                    message = (
+                        'Your boundary file did not pass the initial validation. Cannot submit for admin review. Please verify your data and retry')
+                    send_mail('[S-CAP] - Message about your collection: ' + coll_name, message,
+                              config['EMAIL_HOST_USER'],
+                              [email])
+
             else:
                 message = (
                     'Your data passed the initial validation. Submitted for admin review. Please wait for an email that will let you know '
                     'if the dataset is approved/denied.')
-            send_mail('[S-CAP] - Message about your collection: ' + coll_name, message, config['EMAIL_HOST_USER'],
-                      [email])
+                send_mail('[S-CAP] - Message about your collection: ' + coll_name, message, config['EMAIL_HOST_USER'],
+                          [email])
             # send notification to scap_sysadmins
             approver_group = Group.objects.get(name='scap_sysadmins')
             approvers = approver_group.user_set.all()
